2d9dfeaef30dab38cfc70bd432c37c8ac3b998a6
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2023-09-06 19:23:20 +0200

    tillagt divisionsutrakning och modulo kod

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+//import java.lang.Math;
+
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner tangentbord = new Scanner(System.in);
@@ -12,6 +14,23 @@ public class App {
         int produkt;
         produkt = tal1 * tal2;
         System.out.print(tal1+"*"+tal2+"="+produkt);
+        System.out.println("                    ");
+        System.out.print("Ange decimaltal 1: ");
+        double decimaltal1 = tangentbord.nextDouble();
+        System.out.print("Ange decimaltal 2: ");
+        double decimaltal2 = tangentbord.nextDouble();
+        double kvot;
+        kvot = decimaltal1 / decimaltal2;
+        System.out.print(decimaltal1+"/"+decimaltal2+"="+kvot);
+        System.out.println("                  ");
+        System.out.print("Ange tal 3: ");
+        int tal3 = tangentbord.nextInt();
+        tangentbord.nextLine();
+        System.out.print("Ange tal 4: ");
+        int tal4 = tangentbord.nextInt();
+        tangentbord.nextLine();
+        kvot = tal3 % tal4;
+        System.out.println("Rest av talen "+tal3+"/"+tal4+" = "+kvot);
         tangentbord.close();
     }
 }
