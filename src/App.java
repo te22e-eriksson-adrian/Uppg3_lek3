54aab2bb1c20d84289e94d5d7b81810dabd90002
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2023-09-06 10:56:07 +0200

    gjort en scanner som påverkar en multiplikation

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner tangentbord = new Scanner(System.in);
+        System.out.print("Ange tal 1: ");
+        int tal1 = tangentbord.nextInt();
+        tangentbord.nextLine();
+        System.out.print("Ange tal 2: ");
+        int tal2 = tangentbord.nextInt();
+        tangentbord.nextLine();
+        int produkt;
+        produkt = tal1 * tal2;
+        System.out.print(tal1+"*"+tal2+"="+produkt);
+        tangentbord.close();
     }
 }
